4fbe6351a1488709f7c45d3f4457f4535b701d25
Erica Harmon <ericalaurenharmon@gmail.com>
2023-11-29 21:13:09 -0500

    basic page

--- script.js
+++ script.js
@@ -9,8 +9,24 @@ function initializeGame() {
 // Step 2: Render Board
 function renderBoard() {
     const gameBoard = document.getElementById('game-board');
-    createHTMLCards();
+    createHTMLCards(gameBoard);
     assignUniqueIDs();
     displayBackOfCards();
     document.getElementById('start-game-btn').addEventListener('click', () => playerClickPlay());
+}
+
+function createHTMLCards(gameBoard) {
+    for (let i = 0; i < animalsArray.length * 2; i++) {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.setAttribute('data-animal', animalsArray[Math.floor(i / 2)])
+        card.addEventListener('click', () => handleCardClick(card));
+
+        const cardContent = document.createElement('div');
+        cardContent.className = 'card-content';
+        cardContent.textContent = '?'; //initial hidden content
+
+        card.appendChild(cardContent);
+        gameBoard.appendChild(card);
+    }
 }
