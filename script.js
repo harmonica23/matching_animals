477a0f5f8820ffa08d0fc2646ce8d4c36909cf47
Erica Harmon <ericalaurenharmon@gmail.com>
2023-11-30 14:49:11 -0500

    debugging shuffle array

--- script.js
+++ script.js
@@ -4,10 +4,23 @@ let gameActive = false;
 let flippedCards = [];
 let matchedPairs = 0;
 
+// credit: I did not write this function - it is the Fisher-Yates shuffle algorithm shown to me by chatgpt
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 function initializeGame() {
-    // duplicateArrayToMatchingPairs();
-    // shuffleArray(animalsArray);
+    duplicateArrayToMatchingPairs();
+    shuffleArray(animalsArray);
     renderBoard();
+}
+
+function duplicateArrayToMatchingPairs() {
+    animalsArray = [...animalsArray, ...animalsArray];
 }
 
 // Step 2: Render Board
@@ -22,26 +35,27 @@ function renderBoard() {
 }
 
 function createHTMLCards(gameBoard) {
+    const shuffledAnimals = shuffleArray([...animalsArray, ...animalsArray]);
+
     for (let i = 0; i < animalsArray.length * 2; i++) {
         const card = document.createElement('div');
         card.className = 'card';
-        card.addEventListener('click', handleCardClick);
+        card.addEventListener('click', handleCardClick(card));
 
         const cardContent = document.createElement('div');
         cardContent.className = 'card-content';
-        cardContent.textContent = '?'; //initial hidden content
 
         const cardImage = document.createElement('img') ;
         cardImage.src = 'images/dog.jpg'; // dog is currently linked to test
-        cardImage.alt = 'Hidden';
+        cardImage.alt = 'Hidden'; // hide image until click to flip
 
         cardContent.appendChild(cardImage);
-        // card.appendChild(cardContent);
+        // card.appendChild(cardContent);      <---- if I comment this line in, my dog takes up the entire page
         gameBoard.appendChild(card);
     }
 }
 
-function assignUniqueIDs() {
+function assignUniqueIDs() { // stuck here 
     const cards = document.querySelectorAll('.card');
     cards.forEach((cards, index) => {
         // card.id = `card-${index}`;
@@ -49,22 +63,23 @@ function assignUniqueIDs() {
 }
 
 function displayBackOfCards() {
-
+    // want to keep solid color squares here - not sure what to do
 }
 
-function startGame() {
-
-}
 
 // Step 3: Handle Card Click
+function startGame() {  // how do I shuffle the array upon page load BEFORE click?
+    gameActive = true;
+    shuffleArray(animalsArray);
+}
+
 function handleCardClick(card) {
     if (!gameActive || card.classList.contains('flipped')) {
         return; //do nothing if game is not active or card is already flipped
-    }
-
+    }    
     // check if this is the first click, and if so, start the game
     if (!gameActive) {
-        startGame
+        startGame();
     }
 
     flipCard(card);
@@ -84,8 +99,49 @@ function handleCardClick(card) {
 }
 
 // Step 4: Compare Cards and Check Game Completion
+function compareCards(firstCard, secondCard) {
+    const firstAnimal = firstCard.querySelector('.card-content img').alt;
+    const secondAnimal = secondCard.querySelector('.card-content img').alt;
 
+    if (firstAnimal === secondAnimal) {
+        keepCardsOpen(firstCard, secondCard);
+        matchedPairs++;
+
+        if(allCardsMatched()) {
+            displayGameCompletionMessage();
+            revealPlayAgainButton();
+        }
+    } else {
+        setTimeout(() => {
+            flipBothCardsBack(firstCard, secondCard);
+        }, 1000);
+    }
+}
+
+function keepCardsOpen() {
+
+}
+
+function allCardsMatched() {
+
+}
+
+function displayGameCompletionMessage() {
+
+}
+
+function revealPlayAgainButton() {
+
+}
 
 // Step 5: If player chooses to 'play again'
+function playAgain() {
+
+}
+
+function resetGameBoard() {
+
+}
+
 
 initializeGame(); //this is my last line of code
